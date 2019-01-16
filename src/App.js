1d16fc6e12d94a6e33744faaab5b80009c91bbc2
Nikita Lukjanov <nlukjanov@gmail.com>
2019-01-16 07:51:54 +0000

    added simple Title component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Titles from './components/Titles';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Titles />
+        <p>
+          Yo!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Titles extends Component {
+  render() {
+    return (
+      <div>
+        <p>Weather Finder</p>
+        <p>Find out temperature, conditions and more...</p>
+      </div>
+    );
+  }
+}
+
+export default Titles;
