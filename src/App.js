f3d9d9311767123982e82de06298bbd061cc19e5
Nikita Lukjanov <nlukjanov@gmail.com>
2019-01-19 17:26:25 +0000

    fixed api call in app.js

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ const API_KEY = '2224ff2c958e0fb490213afdb322841e';
 
 class App extends React.Component {
 
-  getWeather = async () => {
-    const api_call = await fetch(`https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}&units=metric`);
+  getWeather = async (e) => {
+    e.preventDefault();
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
+    console.log(data);
   }
 
 
@@ -18,7 +20,7 @@ class App extends React.Component {
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather} />
         <Weather />
       </div>
     );
