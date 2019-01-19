c3c55ae2b02a546fa0678d56323c28942b9f501c
Nikita Lukjanov <nlukjanov@gmail.com>
2019-01-19 17:39:04 +0000

    added setState to app.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ const API_KEY = '2224ff2c958e0fb490213afdb322841e';
 
 class App extends React.Component {
 
+  state = {
+    temperature: '',
+    city: '',
+    country: '',
+    humidity: '',
+    description: '',
+    error: ''
+  }
+
   getWeather = async (e) => {
     e.preventDefault();
 
@@ -17,6 +26,14 @@ class App extends React.Component {
     const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
     console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ''
+    })
   }
 
 
