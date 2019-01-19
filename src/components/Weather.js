3a1608c584c28a379ecb757a2c21285d0a1607cf
Nikita Lukjanov <nlukjanov@gmail.com>
2019-01-19 17:54:10 +0000

    passing data to weather component to view

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,30 @@ class Weather extends Component {
   render() {
     return (
       <div>
-        Weather Component
+        {
+          this.props.city && this.props.country &&
+          <p>
+            Location: {this.props.city}, {this.props.country}
+          </p>
+        }
+        {
+          this.props.temperature &&
+          <p>
+            Temperature: {this.props.temperature}
+          </p>
+        }
+        {
+          this.props.humidity &&
+          <p>
+            Humidity: {this.props.humidity}
+          </p>
+        }
+        {
+          this.props.description &&
+          <p>
+            Condition: {this.props.description}
+          </p>
+        }
       </div>
     );
   }
